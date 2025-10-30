9cc3e9cd4b691ea41d434c1df11f3bce1c7dfd6f
parsa-yz-w <parsa.yz@apptegral.com>
2025-10-30 19:41:21 +0330

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  "content": [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  "theme": {
+    "extend": {
+      "colors": {
+        "whitesmoke": "#f9f9fb",
+        "black": "#000",
+        "gray": "#1e1e1e",
+        "darkgray": "#afafaf",
+        "lightsteelblue": "#cbcdff",
+      },
+      "fontFamily": {
+        "pelak": "Pelak",
+        "inter": "Inter"
+      },
+      "borderRadius": {
+        "num-999": "999px"
+      },
+      "padding": {
+        "num-16": "16px"
+      }
+    },
+    "fontSize": {
+      "num-14": "14px",
+      "num-16": "16px"
+    },
+    "lineHeight": {
+      "num-20": "20px",
+      "num-22": "22px"
+    },
+    "letterSpacing": {
+      "num--0_16": "-0.16px",
+      "num--0_18": "-0.18px"
+    }
+  },
+  "corePlugins": {
+    "preflight": false
+  }
+}
