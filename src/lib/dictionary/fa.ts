6dd5a776b389b54322036acf759e1547c718fc89
mehdi Qasemi <mehdi.q6991@gmail.com>
2025-11-02 18:09:19 +0330

    NF-16:about page created

--- src/lib/dictionary/fa.ts
+++ src/lib/dictionary/fa.ts
@@ -1,5 +1,9 @@
 import { TDictionary } from '../types/index'
 const FA_DICTIONARY: TDictionary = {
-  Home: 'خانه'
+  Home: 'خانه',
+  scrollDown: 'به سمت پایین اسکرول کنید',
+  successfulProjects: 'پروژه موفق',
+  products: 'محصول',
+  articles: 'مقاله'
 }
 export default FA_DICTIONARY
