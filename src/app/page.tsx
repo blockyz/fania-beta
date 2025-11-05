61f617660114cf351681034872b90b7a93b2c24d
parsa-yz-w <parsa.yz@apptegral.com>
2025-11-06 01:15:47 +0330

    Refactor: fix _components folder name inapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import ProductsCategory from "./_components/products-category";
+import ProductsCategory from "./components/products-category";
 
 export default function Home() {
   return (<div className="flex flex-col gap-4"  > <ProductsCategory />
