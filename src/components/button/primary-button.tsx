fae8f20f6fbc183da2736370e4fc96c586588680
mehdi Qasemi <mehdi.q6991@gmail.com>
2025-11-06 17:44:20 +0330

    refactor:just a tiny change to find the error

--- src/components/button/primary-button.tsx
+++ src/components/button/primary-button.tsx
@@ -6,7 +6,7 @@ export const PrimaryButton = ({ className, ...props }: IButtonProps) => {
   return (
     <Button
       {...props}
-      className={`bg-primary hover:bg-primary-hover active:bg-primary-active disabled:bg-primary-disabled disabled:cursor-not-allowed ${className}`}
+      className={`bg-primary hover:bg-primary-hover active:bg-primary-active  disabled:cursor-not-allowed disabled:bg-primary-disabled ${className}`}
     />
   );
 };
