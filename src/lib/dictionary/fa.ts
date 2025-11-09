4d454507d9fe8229f0609507cdb57213030ec18d
Parsa YZ <78669031+blockyz@users.noreply.github.com>
2025-11-09 16:15:10 +0330

    Clean up comments in fa.ts
    Removed unnecessary comments from fa.ts

--- src/lib/dictionary/fa.ts
+++ src/lib/dictionary/fa.ts
@@ -21,12 +21,9 @@ const FA_DICTIONARY: TDictionary = {
   SuccessfulProjects: 'پروژه موفق',
   Articles: 'مقاله',
   AboutUs: 'درباره ما',
-
-  //little part in right//
   Section: "بخش",
   Describe: 'توضیحات',
   Honors: "افتخارات",
-  //main section in detail event//
   Written:"نوشته",
 }
 export default FA_DICTIONARY
