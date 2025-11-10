14852630c0c5c54fa8eb0114d5b912a216a6faef
TinaMahmoudi1 <tinamahmoudi201@gmail.com>
2025-11-09 17:25:33 -0800

    refactor : direction layout in to the rtl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" >
+    <html lang="fa" dir="rtl" >
       <body className={vazirmatn.className}>
         {children}
       </body>
