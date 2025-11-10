aac23c8b5fd65adc2019d1cc1b6053ec9e2bc5bd
Ermia007e <ermiafani1389@gmail.com>
2025-11-10 22:32:25 +0700

    refactor/NF-9/Implement Category Page

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import CategoryMidelSection from './components/Category-midelSection '
-import { CardsSection } from './components/cards-section'
-
-export const Category = () => {
+import React from 'react';
+import CategoryMidelSection from './components/Category-midelSection ';
+import { CardsSection } from './components/cards-section';
+export default function Category() {
   return (
     <>
       <CategoryMidelSection />
       <CardsSection />
     </>
-  )
+  );
 }
